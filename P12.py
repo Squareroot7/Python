db9a39e87ffd4f241c623191bdfc420fbf242456
Lara Devoti <lara.devoti@teamware.it>
2019-04-04 12:22:32 +0200

    Ho creato il primo file che contiene la somma 10

---
+++ P12.py
@@ -0,0 +1,9 @@
+#problema 1.2 programma che visualizzi la somma dei primi 10 numeri interi positivi
+number=1
+result=0
+
+while number!=11:
+	result= result + number
+	number=number+1
+print("La somma dei primi 10 numeri è", result)
+
