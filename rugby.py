ce8dd71d2512201eeac75ac6a8a2a0373e292c89
Lara Devoti <lara.devoti@teamware.it>
2019-06-03 14:33:04 +0200

    rugby.py ENDED, q.py modified file extension

--- rugby.py
+++ rugby.py
@@ -7,9 +7,10 @@ def Rugby(score):
     KICKS=2
     TRIES=3
     CONVERSIONS=7
-    for i in range(100):
-        for j in range(100):
-            for k in range(100):
+
+    for i in range(round((score+1)/2)): #Considering the max number 222 I assured that the rounding would provide the correct maximum number
+        for j in range(round((score+1)/3)):
+            for k in range(round((score+1)/7)): #potrei eliminare il terzo ciclo e vedere se il risultato è divisibile per 7
                 if ((2*i + 3*j + 7*k) == score):
                     print(k,j,i)
 main()
